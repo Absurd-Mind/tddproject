51632f6c3ab223f7e786ac79664a2b81beb1b7b9
Peter Kranz <peter@myref.net>
2023-06-25 16:22:53 +0200

    call write method

--- test/devicedriver/DeviceDriver.test.cpp
+++ test/devicedriver/DeviceDriver.test.cpp
@@ -1,10 +1,16 @@
 #include <DeviceDriver.cpp>
 #include <catch2/catch_all.hpp>
 
-TEST_CASE("drive device") {
+TEST_CASE("Read after device init returns 0") {
     DeviceDriver deviceDriver;
 
     auto result = deviceDriver.read(0x03);
 
     REQUIRE(result == 0);
+}
+
+TEST_CASE("write") {
+    DeviceDriver deviceDriver;
+
+    deviceDriver.write(0x13, 37);
 }
